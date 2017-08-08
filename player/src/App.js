74f062cbf8b7d286d28ef002220386f4794c49cc
someoneoylp <1192575341@qq.com>
2017-08-08 19:24:09 +0800

    loading

--- player/src/App.js
+++ player/src/App.js
@@ -5,7 +5,12 @@ import Header from './components/header/header';
 import MusicList from './components/content/musicList.js';
 import Personal from './components/content/personal.js';
 import Player from './components/content/player.js';
-import Sidebar from './components/sidebar/sidebar.js'
+import Sidebar from './components/sidebar/sidebar.js';
+import classnames from 'classnames'
+import loading from './image/loading.gif';
+
+
+
 import {
   HashRouter,
   Route,
@@ -21,14 +26,22 @@ class App extends Component {
     }
   }
   componentDidMount() {
+      console.log("componentDidMount调用")
     //组件加载完毕调用，设置loading
-    this.state.isLoading = false
+   
+    // this.setState({
+    //     isLoading : false
+    // })
   }
   render() {
+    var loadingClass = classnames({
+        "isShow" : this.state.isLoading
+    });
     return (
       <div className="App">
         <HashRouter>
             <div className="wrapper">
+                <img src={loading} className={loadingClass} />
                 <div className="header">
                     <div class="func">
                         <Link to="/sidebar">
